da6ca76276cf11db1098a3245fa4c7a9c5f33e96
cynthiawjy135 <s225138694@deakin.edu.au>
2025-03-31 00:06:25 +1100

    updated Task4.2P MongoDb

--- Task4.2P_Database/public/js/script.js
+++ Task4.2P_Database/public/js/script.js
@@ -23,7 +23,7 @@ const cardList = [
         '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
         '<div class="card-reveal">'+
         '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
+        '<p class="card-text">'+item.description+'</p>'+
         '</div></div></div>';
         $("#card-section").append(itemToAppend)
         });
